c1f237974e5c8059973f279a14fad67386528baa
c0d3sh0rt4g3 <eriking2004@gmail.com>
2025-02-19 12:58:07 +0100

    Made initial Register structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script>
 import {defineComponent} from "vue";
 import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
 
 export default defineComponent({
-  components: {Login}
+  components: {Register, Login}
 })
 </script>
 
 <template>
   <h1>EatMatch Frontend</h1>
   <Login/>
+  <Register/>
 </template>
 
 <style scoped>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,7 +5,19 @@ export default {
 </script>
 
 <template>
-
+  <form id="register-form">
+    <h2>Register</h2>
+    <label for="name-textbox">Name:</label>
+    <input type="text" name="name" id="name-textbox">
+    <label for="surname-textbox">Surname:</label>
+    <input type="text" name="surname" id="surname-textbox">
+    <label for="email-textbox">Email address:</label>
+    <input type="text" name="email" id="email-textbox">
+    <label for="create-password-textbox">Create password:</label>
+    <input type="password" name="create-password" id="create-password-textbox">
+    <label for="repeat-password-textbox">Repeat password:</label>
+    <input type="password" name="repeat-password" id="repeat-password-textbox">
+  </form>
 </template>
 
 <style scoped>
