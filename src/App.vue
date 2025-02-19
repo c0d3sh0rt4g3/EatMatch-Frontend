c392334da4b7b5be676201ddab8bc6f39b48dee7
c0d3sh0rt4g3 <eriking2004@gmail.com>
2025-02-19 12:45:15 +0100

    Made initial Login structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script>
+import {defineComponent} from "vue";
+import Login from "@/views/Login.vue";
+
+export default defineComponent({
+  components: {Login}
+})
 </script>
 
 <template>
-  <main>
-    <h1>EatMatch Frontend</h1>
-  </main>
+  <h1>EatMatch Frontend</h1>
+  <Login/>
 </template>
 
 <style scoped>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,13 @@ export default {
 </script>
 
 <template>
-
+  <form id="login-form">
+    <h2>Login</h2>
+    <label for="email-textbox">Email address:</label>
+    <input type="text" name="email" id="email-textbox">
+    <label for="password-textbox">Password:</label>
+    <input type="password" name="password" id="password-textbox">
+  </form>
 </template>
 
 <style scoped>
