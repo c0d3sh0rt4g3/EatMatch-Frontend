7eb76183a964dd1ada94d86d02d5bec4510c5af5
out0f4mm0 <eriking2004@gmail.com>
2025-03-05 08:47:10 +0100

    Changed a folder name from views to pages

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import {defineComponent} from "vue";
 import Login from "@/components/Login.vue";
 import Register from "@/components/Register.vue";
 import Header from "@/components/Header.vue";
-import Profile from "@/views/Profile.vue";
+import Profile from "@/pages/Profile.vue";
 
 export default defineComponent({
   components: {Profile, Header, Register, Login}
