2a6f6684923d54b48fe9f7a2162c4d23623f9219
out0f4mm0 <eriking2004@gmail.com>
2025-03-05 08:50:52 +0100

    Added router

---
+++ src/router.js
@@ -0,0 +1,32 @@
+import Home from "@/pages/Home.vue";
+import {createRouter, createWebHistory} from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/profile',
+        component: () => import('./pages/Profile.vue')
+    },
+    {
+        path: '/restaurants',
+        component: () => import('./pages/Restaurants.vue')
+    },
+    {
+        path: '/restaurants/:id',
+        component: () => import('./pages/RestaurantReview.vue')
+    },
+    {
+        path: '/restaurants/:id/write-review',
+        component: () => import('./pages/ReviewWritng.vue')
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
